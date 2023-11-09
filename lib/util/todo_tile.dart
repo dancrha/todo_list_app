f41e92eac688dc9e4a7a3d7dc12003d5414e419c
Dan Crha <44410856+danielcrha@users.noreply.github.com>
2023-11-09 13:06:53 -0500

    first commit

---
+++ lib/util/todo_tile.dart
@@ -0,0 +1,48 @@
+// ignore_for_file: prefer_const_constructors, sort_child_properties_last, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+class ToDoTile extends StatelessWidget {
+  final String taskName;
+  final bool taskCompleted;
+  Function(bool?)? onChanged;
+
+  ToDoTile(
+      {super.key,
+      required this.taskName,
+      required this.taskCompleted,
+      required this.onChanged});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 25.0, right: 25, top: 25),
+      child: Container(
+        padding: EdgeInsets.all(24),
+        child: Row(
+          children: [
+            //checkbox
+            Checkbox(
+              value: taskCompleted,
+              onChanged: onChanged,
+              activeColor: Colors.black,
+            ),
+
+            //task name
+            Text(
+              taskName,
+              style: TextStyle(
+                  decoration: taskCompleted
+                      ? TextDecoration.lineThrough
+                      : TextDecoration.none),
+            ),
+          ],
+        ),
+        decoration: BoxDecoration(
+          color: Colors.yellow,
+          borderRadius: BorderRadius.circular(12),
+        ),
+      ),
+    );
+  }
+}
